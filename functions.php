7b3397549e617b6e42aa7e0a916851fb7a19976c
Dominique Delor <dominique.delor@opsone.net>
2016-06-17 13:45:47 +0200

    init project

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+require_once(get_stylesheet_directory() . '/vendor/autoload.php');
+
+define('TEMPLATE_DIR', get_bloginfo('template_directory'));
+define('TEMPLATE_URL', get_bloginfo('template_url'));
+define('PREFIX', 'OP_');
