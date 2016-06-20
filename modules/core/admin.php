89eb309aceb71249f2ab336573b32c37b606ad20
Dominique Delor <dominique.delor@opsone.net>
2016-06-20 13:23:02 +0200

    clean core setup

--- modules/core/admin.php
+++ modules/core/admin.php
@@ -3,11 +3,7 @@
 function removeDashboardWidgets()
 {
     remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
-    // remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
-    // remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
-    // remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
     remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
-    // remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
     remove_meta_box('dashboard_primary', 'dashboard', 'side');
     remove_meta_box('dashboard_secondary', 'dashboard', 'side');
     remove_meta_box('dashboard_activity', 'dashboard', 'normal');
@@ -37,14 +33,6 @@ function adminBarRender()
     $wp_admin_bar->remove_menu('comments');
     $wp_admin_bar->remove_menu('new-content');
     $wp_admin_bar->remove_menu('wp-logo');
-
-    // $wp_admin_bar->add_menu( array(
-    //     'parent' => false, // use 'false' for a root menu, or pass the ID of the parent menu
-    //     'id' => 'new_media', // link ID, defaults to a sanitized title value
-    //     'title' => "Menu", // link title
-    //     'href' => admin_url( 'nav-menus.php'), // name of file
-    //     'meta' => false // array of any of the following options: array( 'html' => '', 'class' => '', 'onclick' => '', target => '', title => '' );
-    // ));
 }
 
 function menuOrder($menu_ord) {
--- modules/core/images.php
+++ modules/core/images.php
@@ -2,7 +2,7 @@
 
 function customFormat() {
     if ( function_exists('add_theme_support') ) {
-        add_theme_support('post-thumbnails');
+        // add_theme_support('post-thumbnails');
         // set_post_thumbnail_size( 590, 210, true ); // default Post Thumbnail dimensions
         // add_image_size('custom', 1500, 1000, true);
     }
