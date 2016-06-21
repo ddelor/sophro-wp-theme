9372b3e2aa4ad8b697eb542c72ff88ffab0d06da
Dominique Delor <dominique.delor@opsone.net>
2016-06-21 12:54:44 +0200

    contact add validator

--- modules/contact/contactValidator.php
+++ modules/contact/contactValidator.php
@@ -2,11 +2,35 @@
 
 namespace Contact;
 
+use Form\Form;
 use Respect\Validation\Validator as v;
 
 class ContactValidator
 {
-    public function __construct() {
-        
+
+    public function __construct($data) {
+        global $wpdb;
+        parent::__construct();
+
+        $this->data   = $data;
+        $this->fields = array(
+          'user' => array(
+            'value'      => $this->data['user'],
+            'validators' => array('notEmpty'),
+            'errors'     => array('Le nom n\'est pas rempli.'),
+          ),
+          'email' => array(
+            'value'      => $this->data['email'],
+            'validators' => function($value) {
+              return v::email()->validate($value);
+            },
+            'errors'     => array('L\'email est non valide'),
+          ),
+          'message' => array(
+            'value'      => $this->data['message'],
+            'validators' => array('notEmpty'),
+            'errors'     => array('Le message n\'est pas rempli.'),
+          ),
+        );
     }
 }
--- modules/controllers/TemplatePageContactController.php
+++ modules/controllers/TemplatePageContactController.php
@@ -15,10 +15,35 @@ class TemplatePageContactController extends Base
 
         var_dump($_POST);
 
-        $error   = null;
-        $success = null;
+        $error     = false;
+        $success   = false;
+        $base_path = dirname(realpath(__FILE__));
 
-        $contact = new ContactValidator();
+        $contact   = new ContactValidator($_POST);
+
+        if($contact->isValid()){
+            // CREATE EMAIL CONTENT
+            $content   = file_get_contents($base_path .'/../form-contact/contact.html');
+            $content   = str_replace('{message}', $_POST['message'], $content);
+            $content   = str_replace('{user}', $_POST['user'], $content);
+            $content   = str_replace('{email}', $_POST['email'], $content);
+
+            // DEFINE HEADERS
+            $headers   = array('Content-Type: text/html; charset=UTF-8');
+            $headers[] = 'From: '.$_POST['user'].' <' . $_POST['email'] . '>' . "\r\n";
+
+            // SEND EMAIL
+            wp_mail(
+                get_bloginfo('admin_email'), //Email
+                "Demande de contact", //Sujet
+                $content, //Message
+                $headers
+            );
+
+            // CLEAN FORM
+            $_POST = array();
+        }
+        $error = $contact->getErrors();
 
         return array(
             'error'   => $error,
